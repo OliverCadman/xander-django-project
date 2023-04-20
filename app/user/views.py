a0774a367fc6f22d9929d4eb5c3299cc268abad4
Oliver Cadman <o.cadman@live.co.uk>
2023-04-20 16:07:54 +0100

    database config, user model and api

---
+++ app/user/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+from rest_framework import generics
+from user.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    """View to handle creating a user."""
+
+    serializer_class = UserSerializer
